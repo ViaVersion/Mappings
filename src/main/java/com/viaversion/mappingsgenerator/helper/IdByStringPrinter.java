24c7a06c0e1bd2be7b0c425eac75317083852a66
Nassim Jahnke <nassim@njahnke.dev>
2026-09-19 17:34:19 +0200

    Add more utils

--- src/main/java/com/viaversion/mappingsgenerator/helper/IdByStringPrinter.java
+++ src/main/java/com/viaversion/mappingsgenerator/helper/IdByStringPrinter.java
@@ -1,19 +1,27 @@
 package com.viaversion.mappingsgenerator.helper;
 
 import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import com.viaversion.mappingsgenerator.MappingsLoader;
 import java.io.IOException;
 
-public final class IdPrinter {
+public final class IdByStringPrinter {
 
-    private static final String VERSION = "1.19.3";
+    private static final String VERSION = "26.2";
     private static final String DATA_TYPE = "blockstates";
-    private static final int LOOKING_FOR_ID = 1525;
+    private static final String LOOKING_FOR_ID = "white_carpet";
 
     public static void main(final String[] args) throws IOException {
         final JsonObject mappings = MappingsLoader.load("mapping-" + VERSION + ".json");
         final JsonArray array = mappings.getAsJsonArray(DATA_TYPE);
-        System.out.println(array.get(LOOKING_FOR_ID));
+        int i = 0;
+        for (final JsonElement element : array) {
+            if (element.getAsString().equals(LOOKING_FOR_ID)) {
+                System.out.println(i);
+                return;
+            }
+            i++;
+        }
     }
 }
